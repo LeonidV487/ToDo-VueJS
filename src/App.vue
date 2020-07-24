c9eea51e2f4000eb6f5a861a5c8813a6656d7379
Leonid <valeriy92prog@gmail.com>
2020-07-24 11:38:25 +0300

    Final commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <h1 class="ui dividing centered header">Todo App, Leonid Yermak</h1>
+    <div class='ui three column centered grid'>
+      <div class='column'>
+        <todo-list v-bind:todos="todos"></todo-list>
+        <create-todo v-on:create-todo="createTodo"></create-todo>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import sweetalert from 'sweetalert';
+import TodoList from './components/TodoList';
+import CreateTodo from './components/CreateTodo';
+
+export default {
+  name: 'app',
+  components: {
+    TodoList,
+    CreateTodo,
+  },
+  data() {
+    return {
+      todos: [{
+        title: 'Note 1',
+        task: 'Objection A',
+        done: false,
+      }, {
+        title: 'Note 2',
+        task: 'Objection B',
+        done: true,
+      }, {
+        title: 'Note 3',
+        task: 'Objection C',
+        done: false,
+      }],
+    };
+  },
+  methods: {
+    createTodo(newTodo) {
+      this.todos.push(newTodo);
+      sweetalert('Success!', 'To-Do created!', 'success');
+      sweetalert('Success!', 'To-Do created!', 'success');
+    },
+  },
+};
+</script>
